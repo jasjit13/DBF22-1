975f9311edbcce7fa1181d4b08ebd49b4f8164d1
Justin Lou <80555236+justinlouau@users.noreply.github.com>
2022-02-28 10:06:53 +1100

    Deployment script should now be ready

--- deploy/deploy.ino
+++ deploy/deploy.ino
@@ -1,3 +1,15 @@
+#include <Servo.h>
+#include <ServoInput.h>
+
+// Setup radio port
+ServoInputPin<2> channel;
+
+// Setup servo ports and variables
+Servo servo;
+int servoPort1 = D7;
+int servoPort2 = D6;
+int currentDegree = 0;
+
 // Define stepper motor connections and steps per revolution:
 #define dirPin A3
 #define stepPin A4
@@ -5,8 +17,13 @@
 
 void setup() {
   // Setup Serial Monitor
-  Serial.begin(9600); // open the serial port at 9600 bps:
+  // open the serial port at 9600 bps:
+  Serial.begin(9600);
   Serial.print("start");
+
+  // Setup Servo Motors
+  servo1.attach(servoPort1);
+  servo2.attach(servoPort2); 
 
   // Setup Stepper Motor Pins
   pinMode(stepPin, OUTPUT);
@@ -15,12 +32,58 @@ void setup() {
 
 void loop() {
   // Check for signal from radio in a loop
-  boolean state = ch.getBoolean();
-  if (state == true) {
-    Serial.print("signal\n");
-    stepper()
-    delay(3000);
+  boolean deploy = channel.getBoolean();
+  if (deploy == true) {
+    Serial.print("Deploying...\n");
+    stepper();             // Run stepper demo program
+    rotateDegrees(180, 1); // Rotate servo 1 by 180 degrees
+    rotateDegrees(90, 2);  // rotate servo 2 by 90 degrees
+    delay(3000);           // wait 3 seconds to avoid over-deploying
   }
+
+// Rotate the servo to a target degree
+void rotateToDegree(int targetDegree, int servo) {
+  // Normalise the target degree
+  while (targetDegree < 0) targetDegree = targetDegree + 360;
+  targetDegree = targetDegree % 360;
+
+  // Calculate the distancce to target and rotate
+  int degrees = targetDegree - currentDegree;
+  rotateDegrees(degrees, servo);
+}
+
+// Rotate the servo x degrees
+void rotateDegrees(int degrees, int servo) {
+  // Set Constants
+  int forward = 90;
+  int backward = 180;
+  int stationary = 0;
+
+  // Set offsets and directions
+  int offset = 2;
+  int dir = forward;
+  if (degrees < 0){
+    //degrees = degrees * -1;
+    dir = backward;
+  }
+
+  // Rotate the servo
+  if (servo == 1){
+    servo1.write(stationary);
+    delay(degrees*2);
+    servo1.write(dir);
+  }
+  if (servo == 2){
+    servo2.write(stationary);
+    delay(degrees*2);
+    servo2.write(dir);
+  }
+
+  // Update the current degree
+  currentDegree = currentDegree + degrees;
+  if (currentDegree > 360) currentDegree - 360;
+}
+
 
 
 // Run a demo program on the servo motor
